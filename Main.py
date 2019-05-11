726e3de07ebd8340da5b3b997e8efa4f899c3f42
bram <bramdenouden96@hotmail.com>
2019-05-11 13:21:51 +0200

    availabillity test poging 2

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ from random import randint
 
 available = True
 
-if len(sys.argv)>=2:    #check if a com port has been given as argument, else take com 6
+if len(sys.argv) >= 2:  # check if a com port has been given as argument, else take com 6
     port = sys.argv[1]
 else:
     port = '/dev/ttyUSB0'
@@ -21,23 +21,24 @@ except:
     sys.exit("Unable to establish connection at COM port")
 
 CHUNK = 1024
-audio_file_path = os.path.dirname(os.path.abspath(__file__))+ r"/audio/"       # r"C:\Users\bramd\Desktop\MTBE\Lotte\\"
-
-
+audio_file_path = os.path.dirname(os.path.abspath(__file__)) + r"/audio/"  # r"C:\Users\bramd\Desktop\MTBE\Lotte\\"
 
 files = ['01_plakband.wav', '02_WHscream.wav', '03_godveredomme.wav', '04_FACK.wav', '05_FAAAAAACK.wav']
 errorFiles = ['toSmall.wav', 'toBig.wav']
 
+
 def read_serial(ser):
     global available
+    if ~available:
+        return
     print("Reading.....")
     while available:
         reading = ser.readline().decode("utf-8")
         if reading != '':
-             print(reading)
+            print(reading)
         if 'BIEM' in reading:
             print("DING!")
-            selected = randint(0, len(files)-1)
+            selected = randint(0, len(files) - 1)
             print(selected)
             play(audio_file_path + files[selected])
 
@@ -58,7 +59,7 @@ def play(audio_file):
     stream.stop_stream()
     stream.close()
     p.terminate()
-    available=True
+    available = True
 
 
 thread = threading.Thread(target=read_serial, args=(serial_port,))
